cd092867ef6f934208efe631dccd0218e657e3f3
Rifky peratama <2411500013@mahasiwa.atmaluhur.ac.id>
2025-11-07 14:15:58 +0700

    add: buku.php

---
+++ tambah_buku.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once 'config.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $judul = $_POST['judul'];
+    $penulis = $_POST['penulis'];
+    $penerbit = $_POST['penerbit'];
+    $tahun_terbit = $_POST['tahun_terbit'];
+    $stok = $_POST['stok'];
+
+    $sql = "INSERT INTO buku (judul, penulis, penerbit, tahun_terbit, stok) VALUES (?, ?, ?, ?, ?)";
+
+    if($stmt = $mysqli->prepare($sql)) {
+        $stmt->bind_param("sssii", $judul, $penulis, $penerbit, $tahun_terbit, $stok);
+        if($stmt->execute()){
+            header("location: buku.php");
+            exit();
+        }else{
+            echo "Error : Gagal Menyimpan Data";
+        }
+        $stmt->close();
+    };
+    $mysqli->close();
+};
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+        <div class="container mt-4">
+            <h2>Tambah buku baru</h2>
+            <form action="tambah_buku.php" method="post">
+            <form>
+            <div class="mb-3">
+                <label for="judul" class="form-label">judul buku</label>
+                <input type="text" class="form-control" id="judul" name="judul" required>
+            </div>
+            <div class="mb-3">
+                <label for="penulis" class="form-label">penulis</label>
+                <input type="text" class="form-control" id="penulis" name="penulis" required>
+            </div>
+            <div class="mb-3">
+                <label for="penerbit" class="form-label">penerbit</label>
+                <input type="text" class="form-control" id="penerbit" name="penerbit" required>
+            </div>
+            <div class="mb-3">
+                <label for="tahun_terbit" class="form-label">tahun terbit</label>
+                <input type="number" class="form-control" id="tahun_terbit" name="tahun_terbit" required>
+            </div>
+             <div class="mb-3">
+                <label for="stok" class="form-label">stok</label>
+                <input type="number" class="form-control" id="stok" name="stok" required>
+            </div>
+            
+
+            <button type="submit" class="btn btn-primary">Tambah buku</button>
+            <a href="buku.php" class="btn btn-danger">batalkan</a>
+            </form>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
